7d2ea1108637abdf7e92d828bee4d0f20ba58ec9
orventro <orventro@gmail.com>
2022-11-30 13:35:30 +0300

    finished commit

--- SigmaCoefficient.hpp
+++ SigmaCoefficient.hpp
@@ -4,43 +4,40 @@
 #include <stdexcept>
 #include "mfem.hpp"
 
-class SigmaCoefficient : public mfem::Coefficient
+#define p2(a) ((a)*(a))
+
+class SigmaCoefficient : public mfem::VectorCoefficient
 {
 private:
     mfem::GridFunction &u;
-    mfem::Coefficient &lambda, &mu;
+    mfem::Coefficient *lambda, *mu;
     mfem::DenseMatrix eps, sigma;
 
 public:
-    SigmaCoefficient(mfem::GridFunction &_u, mfem::Coefficient &_lambda, mfem::Coefficient &_mu)
-            : u(_u), lambda(_lambda), mu(_mu) { component = -1; }
+    SigmaCoefficient(mfem::GridFunction &_u, mfem::Coefficient *_lambda=0, mfem::Coefficient *_mu=0)
+            :u(_u), lambda(_lambda), mu(_mu), mfem::VectorCoefficient(3) {  }
 
-    virtual double Eval(mfem::ElementTransformation &T, const mfem::IntegrationPoint &ip)
+    void setMaterial(mfem::Coefficient *_lambda, mfem::Coefficient *_mu) {
+        lambda = _lambda;
+        mu = _mu;
+    }
+
+    virtual void Eval(mfem::Vector &V, mfem::ElementTransformation &T, const mfem::IntegrationPoint &ip)
     {
+        MFEM_ASSERT(lambda && mu, "material not set");
+
         u.GetVectorGradient(T, eps);  // eps = grad(u)
         eps.Symmetrize();             // eps = (1/2)*(grad(u) + grad(u)^t)
-        double l = lambda.Eval(T, ip);
-        double m = mu.Eval(T, ip);
+        double l = lambda->Eval(T, ip);
+        double m = mu->Eval(T, ip);
         sigma.Diag(l*eps.Trace(), eps.Size()); // sigma = lambda*trace(eps)*I
         sigma.Add(2*m, eps);          // sigma += 2*mu*eps
 
-        switch (component)
-        {
-            case 0:
-                return sigma(0, 0); // Sxx
-            case 1:
-                return sigma(0, 1); // Sxy
-            case 2:
-                return sigma(1, 1); // Syy
-            default:
-                throw std::runtime_error("Only 0(Sxx), 1(Sxy) and 2(Syy) are supported.");
-        }
+        V(0) = (sigma(0,0) + sigma(1,1))/2 + 1/2*std::sqrt(p2(sigma(0,0) - sigma(1,1)) + 4*p2(sigma(0,1)));
+        V(1) = std::min(V(1), V(0));
+        V(2) = std::max(V(2), V(0));
     }
     
     virtual void Read(std::istream &in) { }
     virtual ~SigmaCoefficient() { }
-    void setComponent (char _component) { component = _component; }
-
-private:
-    char component;
 };
